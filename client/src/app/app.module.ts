8ff3961ce00d6b7af772ba84f75b4b10b3292250
stm32p103 <stm32p103@gmail.com>
2019-08-15 17:53:21 +0900

    primeng blueprintコピー

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routes';
+import { AppMenuComponent, AppSubMenuComponent} from './app.menu.component';
+import { AppTopBarComponent } from './app.topbar.component';
+import { AppFooterComponent } from './app.footer.component';
+import { DashboardComponent } from './pages/dashboard.component';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutes,
+        ScrollPanelModule,
+        ButtonModule
+    ],
+    declarations: [
+        AppComponent,
+        AppMenuComponent,
+        AppSubMenuComponent,
+        AppTopBarComponent,
+        AppFooterComponent,
+        DashboardComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
